82a24adb06bc9eb0e20f66ea1366f22f7b5a013e
Daniel <danielfernandes202@gmail.com>
2023-09-20 11:24:19 -0300

    insert custom User in model sector

--- backend/sector/models.py
+++ backend/sector/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Sector(models.Model):
@@ -6,3 +7,26 @@ class Sector(models.Model):
 
     def __str__(self) -> str:
         return self.name
+    
+
+
+class CustomUser(AbstractUser):
+    sector = models.ForeignKey(Sector, on_delete=models.SET_NULL, null=True, blank=True)
+
+    # Fornecer related_name exclusivo para evitar colisões
+    groups = models.ManyToManyField(
+        "auth.Group",
+        verbose_name="Groups",
+        blank=True,
+        related_name="custom_users"
+    )
+
+    user_permissions = models.ManyToManyField(
+        "auth.Permission",
+        verbose_name="User permissions",
+        blank=True,
+        related_name="custom_users"
+    )
+
+    def __str__(self):
+        return self.username
