4b63e9286cda6865822133952c142ade08d5c923
Daniel <danielfernandes202@gmail.com>
2023-09-20 11:23:53 -0300

    remove app models

--- backend/app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-from sector.models import Sector  # Substitua 'your_app_name' pelo nome do seu aplicativo
-
-class CustomUser(AbstractUser):
-    sector = models.ForeignKey(Sector, on_delete=models.SET_NULL, null=True, blank=True)
-
-    def __str__(self):
-        return self.username
