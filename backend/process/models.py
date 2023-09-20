b5b66b140d3739812b131a18ed3ae86539c579fb
Daniel <danielfernandes202@gmail.com>
2023-09-20 10:47:10 -0300

    fix model process

--- backend/process/models.py
+++ backend/process/models.py
@@ -6,8 +6,8 @@ from sector.models import Sector
 class Process(models.Model):
     process_number = models.CharField("Numero do Processo", max_length=255)
     object = models.CharField("Objeto", max_length=255)
-    sector = models.ForeignKey(verbose_name="Setor", to=Sector, on_delete=models.SET_NULL, null=True)
-    status = models.BooleanField("Recebido pela unidade?", bool, default=False)
+    sector = models.ForeignKey(Sector, verbose_name="Setor", on_delete=models.SET_NULL, null=True)
+    status = models.BooleanField("Recebido pela unidade?", default=False)
 
     def __str__(self) -> str:
         return self.process_number
