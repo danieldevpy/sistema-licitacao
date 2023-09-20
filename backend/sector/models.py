81b7048f32eaa6fcebe2469e560a12d30a2dbc28
Daniel <danielfernandes202@gmail.com>
2023-09-20 10:20:22 -0300

    configure representation name model sector

--- backend/sector/models.py
+++ backend/sector/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class Sector(models.Model):
     name = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name
