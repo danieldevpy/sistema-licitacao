acd11be86967df4de489be5f887e294427e44df6
Daniel <danielfernandes202@gmail.com>
2023-09-20 10:19:40 -0300

    register sector site admin

--- backend/sector/admin.py
+++ backend/sector/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Sector
 # Register your models here.
+class SectorAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Sector, SectorAdmin)
