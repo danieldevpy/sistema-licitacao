b4864a7220d29eceed63635a7b386d0488c90daf
Daniel <danielfernandes202@gmail.com>
2023-09-20 10:44:14 -0300

    create model process

--- backend/process/models.py
+++ backend/process/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from sector.models import Sector
 
 # Create your models here.
+
+class Process(models.Model):
+    process_number = models.CharField("Numero do Processo", max_length=255)
+    object = models.CharField("Objeto", max_length=255)
+    sector = models.ForeignKey(verbose_name="Setor", to=Sector, on_delete=models.SET_NULL, null=True)
+    status = models.BooleanField("Recebido pela unidade?", bool, default=False)
+
+    def __str__(self) -> str:
+        return self.process_number
