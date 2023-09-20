103b896b10c927e75b4001bcc2b0d2a9e5d81437
Daniel <danielfernandes202@gmail.com>
2023-09-20 10:44:55 -0300

    configure process admin

--- backend/process/admin.py
+++ backend/process/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Process
 # Register your models here.
+
+
+class ProcessAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Process, ProcessAdmin)
