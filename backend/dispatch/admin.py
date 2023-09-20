724da5ff300a66d58528f6539c7f7bc4567bacc6
Daniel <danielfernandes202@gmail.com>
2023-09-20 11:12:27 -0300

    configure admin dispatch

--- backend/dispatch/admin.py
+++ backend/dispatch/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Dispatch
 # Register your models here.
+
+
+class AdminDispatch(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Dispatch, AdminDispatch)
