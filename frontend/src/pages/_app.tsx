937698af5320258f9ff76a3a662d64fb8974f366
Daniel <danielfernandes202@gmail.com>
2023-09-21 13:26:14 -0300

    page global

---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import "../app/globals.css"
+import React from 'react';
+import { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+    return <Component {...pageProps} />;
+}
+
+export default MyApp;
